cde5d6aff930dd99c4ebd0cf2d67db003402f500
manucho <manuel.blanco77@gmail.com>
2020-12-18 16:36:51 -0300

    Messages in /components. modularizing components

---
+++ src/components/Messages.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { IncomingMessage } from './IncomingMessage';
+import { OutgoingMessage } from './OutgoingMessage';
+import { SendMessages } from './SendMessages';
+
+export const Messages = () => {
+
+    const msgs = [1,2,3,4,5,6,7,8,9,10];
+
+    return (
+        <div className="mesgs">
+
+                    {/* <!-- Historia inicio --> */}
+                    <div className="msg_history">
+                        {
+                            msgs.map( (msg) => (
+                                (msg % 2)
+                                    ? <IncomingMessage key={msg}/>
+                                    : <OutgoingMessage key={msg}/>
+                            ))
+                        }
+
+
+                    
+                    </div>
+                {/* <!-- Historia Fin --> */}
+
+                <SendMessages />
+
+                </div>
+    )
+}
