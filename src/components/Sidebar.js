ce4c7f4e48f02e20d34ee8daa25d00d43eea5926
manucho <manuel.blanco77@gmail.com>
2020-12-18 15:02:14 -0300

    Definicion del chat en /components, +css de chat

---
+++ src/components/Sidebar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { SidebarChatItem } from './SidebarChatItem';
+
+export const Sidebar = () => {
+
+    const chats = [1,2,3,4,5,6,7,8,9];
+
+    return (
+        <div className="inbox_chat">
+
+            {
+                chats.map( (chat) => {
+                    return <SidebarChatItem key={ chat }/>
+                })
+            }
+
+
+            {/* <!-- Espacio extra para scroll --> */}
+            <div class="extra_space"></div>
+        </div>
+    )
+}
