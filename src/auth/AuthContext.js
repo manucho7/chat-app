fa1beb2228540bea203e58c916b26050e4592894
manucho <manuel.blanco77@gmail.com>
2020-12-21 14:32:30 -0300

    Disabilitando boton de ingresar-loginpage en el caso en que campos no contengan nada

--- src/auth/AuthContext.js
+++ src/auth/AuthContext.js
@@ -32,6 +32,8 @@ export const AuthProvider = ({ children }) => {
                 name: usuario.nombre,
                 email: usuario.email
             });
+            
+            console.log('autenticado');
         }
         return resp.ok;
     }
