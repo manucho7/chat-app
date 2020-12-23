b3cf3a31c0e8f212339088e5c0d71b80251ba8c3
manucho <manuel.blanco77@gmail.com>
2020-12-23 09:49:49 -0300

    Public and Private Routes which are managed by the AppRouter component

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -2,16 +2,17 @@ import React, { useContext, useEffect } from 'react';
 import {
     BrowserRouter as Router,
     Switch,
-    Route,
     Redirect,
   } from 'react-router-dom';
 import { AuthContext } from '../auth/AuthContext';
 import { ChatPage } from '../pages/ChatPage';
 import { AuthRouter } from './AuthRouter';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
 
-    const {auth, verificarToken} = useContext(AuthContext);
+    const { auth, verificarToken } = useContext(AuthContext);
 
     useEffect(() => {
         verificarToken();
@@ -25,8 +26,8 @@ export const AppRouter = () => {
         <Router>
             <div>
                 <Switch>
-                    <Route path="/auth" component={ AuthRouter } />
-                    <Route exact path="/" component={ ChatPage } />
+                    <PublicRoute isAuthenticated={ auth.logged } path="/auth" component={ AuthRouter } />
+                    <PrivateRoute isAuthenticated={ auth.logged } exact path="/" component={ ChatPage } />
 
                     <Redirect to="/" />
                 </Switch>
