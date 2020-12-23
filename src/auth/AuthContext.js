4088064286e645b15811449124f336320bd6f7cb
manucho <manuel.blanco77@gmail.com>
2020-12-23 14:36:05 -0300

    Cerrar sesion en AuthContext > logout + agregar nombre de usuario en searchbox component

--- src/auth/AuthContext.js
+++ src/auth/AuthContext.js
@@ -107,6 +107,12 @@ export const AuthProvider = ({ children }) => {
 
     const logout = () => {
 
+        localStorage.removeItem('token');
+        
+        setAuth({
+            checking: false,
+            logged: false
+        });
     }
 
     return (
