259d55b6082ccafe4b4cab7072c2ee1534e083d2
manucho <manuel.blanco77@gmail.com>
2021-01-04 10:34:33 -0300

    Sending messages

--- src/components/SendMessages.js
+++ src/components/SendMessages.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const SendMessages = () => {
+
+    const [ message, setMessage ] = useState('');
+
+    const onChange = ({ target }) => {
+        setMessage( target.value );
+    }
+
+    const onSubmit = (ev) => {
+        ev.preventDefault();
+
+        if ( message === 0 ){ return; }
+
+        setMessage('');
+
+        //TODO: Emitir un evento de socket para enviar el message
+        //Hacer el dispatch del message
+    }
+
     return (
-        <form>
+        <form onSubmit={ onSubmit } >
             <div className="type_msg row">
                 <div className="input_msg_write col-sm-9">
-                    <input type="text" className="write_msg" placeholder="Mensaje..." />
+
+                    <input 
+                        type="text" 
+                        className="write_msg" 
+                        placeholder="Mensaje..."
+                        value={ message }
+                        onChange={ onChange }
+                    />
+
                 </div>
                 <div className="col-sm-3 text-center">
                     <button className="msg_send_btn mt-3" type="submit">
